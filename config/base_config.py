7793207cbe28c9a89b08a9fcec171b19364820f1
Liu Chao <bradleon@163.com>
2025-08-07 16:11:50 +0800

    无所谓的变更

--- config/base_config.py
+++ config/base_config.py
@@ -13,7 +13,7 @@
 PLATFORM = "xhs"
 #KEYWORDS = "丰盈蓬松洗发水,livingproof,缕灵, 塞那耳机"  # 关键词搜索配置，以英文逗号分隔
 #KEYWORDS = "livingproof,缕灵, 塞那耳机"  # 关键词搜索配置，以英文逗号分隔
-KEYWORDS = "塞那耳机"  # 关键词搜索配置，以英文逗号分隔
+KEYWORDS = "抗老眼霜,贵妇眼霜,白金眼霜"  # 关键词搜索配置，以英文逗号分隔
 LOGIN_TYPE = "qrcode"  # qrcode or phone or cookie
 COOKIES = ""
 # 具体值参见media_platform.xxx.field下的枚举值，暂时只支持小红书
@@ -58,7 +58,7 @@ USER_DATA_DIR = "%s_user_data_dir"  # %s will be replaced by platform name
 START_PAGE = 1
 
 # 爬取视频/帖子的数量控制
-CRAWLER_MAX_NOTES_COUNT = 100
+CRAWLER_MAX_NOTES_COUNT = 20
 
 # 并发爬虫数量控制
 MAX_CONCURRENCY_NUM = 2
